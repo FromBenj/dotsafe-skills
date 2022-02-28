6e14c83caab067f5e13ee66039b627942f507277
Benjamin TOUBOUL <touboul.benjamin@gmail.com>
2022-02-28 12:09:42 +0100

    custom member contributions API

--- dotsafe-skills-API/src/Controller/MemberProjects.php
+++ dotsafe-skills-API/src/Controller/MemberProjects.php
@@ -4,7 +4,32 @@
 namespace App\Controller;
 
 
-class MemberProjects
-{
+use App\Entity\Member;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
+class MemberProjects extends AbstractController
+{
+    protected $em;
+
+    public function __construct(EntityManagerInterface $em) {
+        $this->em = $em;
+    }
+
+    public function __invoke(Member $data): array
+    {
+        $projects = [];
+        $projectsIds = [];
+        foreach($data->getContributions() as $contribution) {
+            if (!in_array($contribution->getProject()->getId(), $projectsIds, true)) {
+                $projects[] = $contribution->getProject();
+            }
+        }
+        $a = $data->getContributions();
+        dd(["technology" => $a[1]->getTechnology(),
+        []
+        );
+
+        return $projects;
+    }
 }
